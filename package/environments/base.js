03080c4b299df5c7bba22221618890fa9123baed
qinmingyuan <mingyuan0715@foxmail.com>
2021-06-17 17:04:09 +0800

    add plugins

--- package/environments/base.js
+++ package/environments/base.js
@@ -4,11 +4,36 @@
 const { join } = require('path')
 const config = require('../config')
 const rootPath = join(config.root_path)
-const getEntryObject = require('../paths')
+const getEntryObject = require('../utils')
+
+const resolvedPath = (packageName) => {
+  try {
+    return require.resolve(packageName)
+  } catch (e) {
+    if (e.code !== 'MODULE_NOT_FOUND') {
+      throw e
+    }
+    return null
+  }
+}
+
+const getPlugins = () => {
+  const plugins = []
+
+  if (resolvedPath('vite-plugin-compression')) {
+    const viteCompression = 'vite-plugin-compression'
+    plugins.push(
+      viteCompression()
+    )
+  }
+
+  return plugins
+}
 
 module.exports = {
   root: join(process.cwd(), config.root_path),
   entry_paths: config.entry_paths,
+  plugins: getPlugins(),
   resolve: {
     alias: config.alias
   },
