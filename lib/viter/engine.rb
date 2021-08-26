4ddbfea5df7a861b0709abf66ecb6f35ef31db4a
MingyuanQin <mingyuan0715@foxmail.com>
2021-08-26 11:10:40 +0800

    moved spring

--- lib/viter/engine.rb
+++ lib/viter/engine.rb
@@ -8,11 +8,6 @@ module Viter
     initializer 'viter.bootstrap' do
       if defined?(Rails::Server) || defined?(Rails::Console)
         Viter.bootstrap
-        if defined?(Spring)
-          require 'spring/watcher'
-          Spring.after_fork { Viter.bootstrap }
-          Spring.watch(Viter.config.config_path)
-        end
       end
     end
 
